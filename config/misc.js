7a6dfdf6ce760c9e1d6d39498a2a69fa1ecaf0f8
Shivam Maheshwari <shivam@canbrand.in>
2015-10-08 20:35:35 +0530

    Set the default port of this app to 2203

---
+++ config/misc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /***************************************************************************
+   * The `port` setting determines which TCP port your app will be           *
+   * deployed on.                                                            *
+   *                                                                         *
+   * Ports are a transport-layer concept designed to allow many different    *
+   * networking applications run at the same time on a single computer.      *
+   * More about ports:                                                       *
+   * http://en.wikipedia.org/wiki/Port_(computer_networking)                 *
+   *                                                                         *
+   * By default, if it's set, the app uses the `PORT` environment variable.  *
+   * Otherwise it falls back to port 2203.                                   *
+   *                                                                         *
+   * In env/production.js, you'll probably want to change this setting       *
+   * to 80 (http://) or 443 (https://) if you have an SSL certificate        *
+   ***************************************************************************/
+
+  port: process.env.PORT || 2203
+
+};
