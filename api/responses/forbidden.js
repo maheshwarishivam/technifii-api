280d3ae47ca8d5ed073466e9352fea1be05dac9c
Shivam Maheshwari <shivam@canbrand.in>
2015-10-08 06:05:49 +0530

    Modified responses to always return JSON and removed corresponding views.
    Left layout.ejs with only body for just in case scenarios

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -3,16 +3,22 @@
  *
  * Usage:
  * return res.forbidden();
- * return res.forbidden(err);
- * return res.forbidden(err, 'some/specific/forbidden/view');
+ * return res.forbidden(message);
+ * return res.forbidden(message,data);
+ * return res.forbidden(message,data,error);
  *
  * e.g.:
  * ```
  * return res.forbidden('Access denied.');
  * ```
+ *
+ * NOTE:
+ *
+ * error, if passed, is shown only if the environment is not production.
+ * It is, however, always logged
  */
 
-module.exports = function forbidden (data, options) {
+module.exports = function forbidden (message, data, error) {
 
   // Get access to `req`, `res`, & `sails`
   var req = this.req;
@@ -23,8 +29,8 @@ module.exports = function forbidden (data, options) {
   res.status(403);
 
   // Log error to console
-  if (data !== undefined) {
-    sails.log.verbose('Sending 403 ("Forbidden") response: \n',data);
+  if (error !== undefined) {
+    sails.log.error('Sending 403 ("Forbidden") response: \n',error);
   }
   else sails.log.verbose('Sending 403 ("Forbidden") response');
 
@@ -32,46 +38,20 @@ module.exports = function forbidden (data, options) {
   // is not set to 'production'.  In production, we shouldn't
   // send back any identifying information about errors.
   if (sails.config.environment === 'production') {
-    data = undefined;
+    error = undefined;
   }
 
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
+  var response = {
+    status: 403,
+    message: (message != undefined && message !== null)?message:'404 Not Found',
+    data: (data != undefined && data !== null)?data:{}
+  };
+
+  if(error != undefined) {
+    response.error = error;
   }
 
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  else return res.view('403', { data: data }, function (err, html) {
-
-    // If a view error occured, fall back to JSON(P).
-    if (err) {
-      //
-      // Additionally:
-      // • If the view was missing, ignore the error but provide a verbose log.
-      if (err.code === 'E_VIEW_FAILED') {
-        sails.log.verbose('res.forbidden() :: Could not locate view for error page (sending JSON instead).  Details: ',err);
-      }
-      // Otherwise, if this was a more serious error, log to the console with the details.
-      else {
-        sails.log.warn('res.forbidden() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
-      }
-      return res.jsonx(data);
-    }
-
-    return res.send(html);
-  });
-
+  //always respond with JSON
+  return res.jsonx(response);
 };
 
--- api/responses/notFound.js
+++ api/responses/notFound.js
@@ -3,8 +3,9 @@
  *
  * Usage:
  * return res.notFound();
- * return res.notFound(err);
- * return res.notFound(err, 'some/specific/notfound/view');
+ * return res.notFound(message);
+ * return res.notFound(message,data);
+ * return res.notFound(message,data,error);
  *
  * e.g.:
  * ```
@@ -12,12 +13,16 @@
  * ```
  *
  * NOTE:
+ *
+ * error, if passed, is shown only if the environment is not production.
+ * It is, however, always logged
+ *
  * If a request doesn't match any explicit routes (i.e. `config/routes.js`)
  * or route blueprints (i.e. "shadow routes", Sails will call `res.notFound()`
  * automatically.
  */
 
-module.exports = function notFound (data, options) {
+module.exports = function notFound (message, data, error) {
 
   // Get access to `req`, `res`, & `sails`
   var req = this.req;
@@ -28,8 +33,8 @@ module.exports = function notFound (data, options) {
   res.status(404);
 
   // Log error to console
-  if (data !== undefined) {
-    sails.log.verbose('Sending 404 ("Not Found") response: \n',data);
+  if (error !== undefined) {
+    sails.log.error('Sending 404 ("Not Found") response: \n',error);
   }
   else sails.log.verbose('Sending 404 ("Not Found") response');
 
@@ -37,46 +42,20 @@ module.exports = function notFound (data, options) {
   // is not set to 'production'.  In production, we shouldn't
   // send back any identifying information about errors.
   if (sails.config.environment === 'production') {
-    data = undefined;
+    error = undefined;
   }
 
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
+  var response = {
+    status: 404,
+    message: (message != undefined && message !== null)?message:'404 Not Found',
+    data: (data != undefined && data !== null)?data:{}
+  };
+
+  if(error != undefined) {
+    response.error = error;
   }
 
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  else return res.view('404', { data: data }, function (err, html) {
-
-    // If a view error occured, fall back to JSON(P).
-    if (err) {
-      //
-      // Additionally:
-      // • If the view was missing, ignore the error but provide a verbose log.
-      if (err.code === 'E_VIEW_FAILED') {
-        sails.log.verbose('res.notFound() :: Could not locate view for error page (sending JSON instead).  Details: ',err);
-      }
-      // Otherwise, if this was a more serious error, log to the console with the details.
-      else {
-        sails.log.warn('res.notFound() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
-      }
-      return res.jsonx(data);
-    }
-
-    return res.send(html);
-  });
-
+  //always respond with JSON
+  return res.jsonx(response);
 };
 
